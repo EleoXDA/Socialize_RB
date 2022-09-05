e6efe6c2e148d45eef1c66fb4bd0bd07fec211ca
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-05 16:00:44 +0100

    Added lang, user and user_lang seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "Deleting previous seeds..."
+Language.destroy_all
+User.destroy_all
+UserLanguage.destroy_all
+
+puts "Creating languages..."
+languages = ['Java', 'Kotlin', 'Ruby', 'Javascript', 'Swift', 'Dart', 'CSS',
+  'HTML', 'Bash', 'XML', 'C++', 'Rust', 'Pascal', 'Fortran', 'PHP',
+  'Perl']
+
+languages.each do |language|
+  language = Language.new(name: language)
+  language.save!
+end
+
+puts "Creating users..."
+20.times do
+  user = User.new(nickname: Faker::Beer.brand, email: Faker::Internet.email, password: "123456")
+  user.save!
+  # puts "Creating user languages..."
+  user_language = UserLanguage.new(language: Language.all.sample, user: user)
+  user_language.save!
+end
