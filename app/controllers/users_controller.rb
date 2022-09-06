d93c91cd3197ffca7eeb732f1e05b27aaaad7bcc
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 13:57:56 +0200

    Add location and info window to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  @markers = @users.geocoded.map do |user|
+    {
+      lat: user.latitude,
+      lng: user.longitude,
+      info_window: render_to_string(partial: "info_window", locals: {user: user})
+    }
+  end
 end
