d40b104857ef552db8524ec699f9a17624ed1a2c
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 18:06:30 +0200

    FIX: info window issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { user: user }),
+        info_window: render_to_string(partial: "info_window", locals: { user: user })
         # image_url: helpers.asset_url("laptop.png")
       }
     end
