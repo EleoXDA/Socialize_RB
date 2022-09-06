7fef8b1ecbae0561c439f82d520256f0adcf9309
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 14:05:44 +0200

    ADD index action to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  @markers = @users.geocoded.map do |user|
-    {
-      lat: user.latitude,
-      lng: user.longitude,
-      info_window: render_to_string(partial: "info_window", locals: {user: user})
-    }
+
+
+  def index
+    @users = User.all
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {user: user})
+      }
+    end
   end
 end
