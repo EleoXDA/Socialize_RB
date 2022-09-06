3fb3e069d65becbdd14771fe1c009435188a1e33
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 14:45:37 +0200

    EDIT: comment out info image for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-
-
   def index
     @users = User.all
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window: render_to_string(partial: "info_window", locals: {user: user})
+        info_window: render_to_string(partial: "info_window", locals: user)
+        # image_url: helpers.asset_url("")
       }
     end
   end
