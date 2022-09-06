a9eea331d40d704460d4a3d89ebb7983846a2bf3
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 14:00:50 +0200

    Add location to Users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_103140) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_115331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_103140) do
     t.string "nickname"
     t.float "latitude"
     t.float "longitude"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
