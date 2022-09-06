ed18006771a4b543d37a0d5dd86b6a3cebab6758
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-06 15:38:39 +0100

    Pin request mechanism added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
     @chat_requests = ChatRequest.where(asker: current_user).or(ChatRequest.where(receiver: current_user))
     @chat_requests = @chat_requests.filter{ |chat| chat.status == 1 }
+
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
