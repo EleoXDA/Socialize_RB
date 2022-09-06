2d77d8a069d370deadc75349245b11b50a3301cb
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 17:21:03 +0200

    FIX: geocofing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window: render_to_string(partial: "info_window", locals: user)
-        # image_url: helpers.asset_url("")
+        info_window: render_to_string(partial: "info_window", locals: { user: user }),
+        # image_url: helpers.asset_url("laptop.png")
       }
     end
   end
