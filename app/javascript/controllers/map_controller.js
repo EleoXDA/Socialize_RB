956f5b8de7f047add6d3689d03db714c6629b848
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 14:46:06 +0200

    EDIT map controller and update index.js

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -17,6 +18,8 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     // this.#fitMapToMarkers()
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -29,5 +32,5 @@ export default class extends Controller {
   //   const bounds = new mapboxgl.LngLatBounds()
   //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
   //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  }
+// }
 }
