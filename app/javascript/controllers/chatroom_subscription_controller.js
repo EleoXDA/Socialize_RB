a49c51b9faca65e7a15023706632385e08b5d72c
Nirajan Wagle <nirajanwagle2@gmail.com>
2022-09-06 22:45:24 +0200

    FIX: Form updates without refreshing

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -5,21 +5,21 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable";
 
 export default class extends Controller {
-  static values = { chat_roomID: Number }
+  static values = { chatroomId: Number }
   static targets = ["messages"]
 
   connect() {
-    this.channel = consumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chat_roomIdValue },
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => this.#insertMessageAndScrollDown(data) }
     )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+    // console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
   #insertMessageAndScrollDown(data) {
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0)
-    // this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
   resetForm(event) {
