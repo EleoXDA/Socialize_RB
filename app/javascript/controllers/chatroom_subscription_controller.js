320cb34bf851b88caeb2d92355e2cf6083ab3a78
Nirajan Wagle <nirajanwagle2@gmail.com>
2022-09-06 17:09:04 +0200

    not responsive at all but works

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -11,8 +11,22 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: data => this.#insertMessageAndScrollDown(data) }
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 # puts "Deleting previous seeds..."
-
 UserLanguage.destroy_all
 Language.destroy_all
 puts "Language destroyeD!"
@@ -17,6 +16,8 @@ ChatRequest.destroy_all
 User.destroy_all
 
 puts "Users destroyed!"
+
+
 
 
 puts "Creating languages..."
@@ -46,11 +47,10 @@ asker = USER.split(9).sample
 receiver = USER.split(9).sample
 
 for i in 0...asker.size do
-  chat_request = ChatRequest.new(asker: asker[i], receiver: receiver[i], status: 1)
+  chat_request = ChatRequest.new(asker: asker[i], receiver: receiver[i], accepted: true, pinned: false)
   chat_request.save!
 
-  chat_room = ChatRoom.new(name: "general")
-
+  chat_room = ChatRoom.new(name: "general", chat_request: chat_request)
   chat_room.save!
 end
 
