cf0a4fcae364ff4867fd3d79f526410c618d51a2
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 21:04:56 +0200

    Autocomplete + Search for Maps

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -13,6 +13,15 @@ export default class extends Controller {
       types: "country,region,place,postcode,locality,neighborhood,address"
     })
     this.geocoder.addTo(this.element)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+  }
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
   }
   disconnect() {
     this.geocoder.onRemove()
