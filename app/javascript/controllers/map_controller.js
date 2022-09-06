2d474dbf93c519f97c745fe58e2e9b2dd59a1dd7
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 13:58:26 +0200

    ADD Javascipt logic

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+    this.#addMarkersToMap()
+    // this.#fitMapToMarkers()
+  }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    })
+  }
+  // #fitMapToMarkers() {
+  //   const bounds = new mapboxgl.LngLatBounds()
+  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+}
