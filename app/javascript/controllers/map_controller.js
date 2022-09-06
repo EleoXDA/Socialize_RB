cc5bfa7fc16b90cbcc8f5979aa764e0ccd5efb65
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-06 23:18:45 +0200

    Update map properties

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v11"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
@@ -33,6 +33,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 10, maxZoom: 50, duration: 0 })
   }
 }
