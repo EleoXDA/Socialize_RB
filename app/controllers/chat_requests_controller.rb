78ddaaee739b856d78eae248eec82abc432d6424
Elena Pegoraro <elenapoikilia@gmail.com>
2022-09-08 12:26:19 +0200

    redirecting to index page when accepting request

--- app/controllers/chat_requests_controller.rb
+++ app/controllers/chat_requests_controller.rb
@@ -22,9 +22,9 @@ class ChatRequestsController < ApplicationController
       if @chat_request.confirmed? # update status
         # raise
         @chat_room = ChatRoom.create(chat_request: @chat_request)
-        redirect_to @chat_room, status: :see_other
+        redirect_to users_path, status: :see_other
       end
-      redirect_to users_path, status: :see_other
+     # redirect_to users_path, status: :see_other
     else
       redirect_to action: "index", status: :unprocessable_entity
     end
