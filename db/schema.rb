53fd680a60de3403f7c4b111d3853ba9748c36f4
Elena Pegoraro <elenapoikilia@gmail.com>
2022-09-12 14:40:43 +0200

    icons working

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_12_084058) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_084058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "vonage_session_id"
     t.index ["chat_request_id"], name: "index_chat_rooms_on_chat_request_id"
   end
 
