bdfc24b5307c63913274828741c0a8aba8817980
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-12 11:38:55 +0100

    Added redirect page for adding location which then redirects to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,20 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = current_user
   end
 
   def update
-
+    @user = current_user
+    @user.update(sign_up_params)
+    redirect_to users_path
   end
 
   private
+
+  def sign_up_params
+    params.require(:user).permit(:location)
+  end
 
   def user_params
     params.require(:user).permit(:photo, :location)
