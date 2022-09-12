8d579b7eb69d8e3bcaf606b44ae9f54a2e77a4c9
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-12 10:04:42 +0100

    Working github auth with image and profile urls

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,6 @@
 # This has been added manually (not in terminal) - could be
 # a source of error later on
+
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_114607) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_084058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_114607) do
     t.string "name", default: "", null: false
     t.string "provider"
     t.string "uid"
+    t.string "image"
+    t.string "html_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
