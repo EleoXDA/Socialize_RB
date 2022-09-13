9907e26df59f96de0c930bc194ad80ca16b5282b
Nirajan Wagle <nirajanwagle2@gmail.com>
2022-09-13 16:30:35 +0200

    Fixes the type

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,8 +8,8 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log(this.apiKeyValue)
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v11"
