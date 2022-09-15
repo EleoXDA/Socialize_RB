e06b3158f9ba0ebf4d6199b46478f4bb9fa6a1db
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-15 10:46:19 +0100

    Adding photos to event create

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+require "open-uri"
+
 class Event < ApplicationRecord
+  after_create :set_default_event_avatar
   has_one_attached :photo
+
+  def set_default_event_avatar
+    return if photo.attached?
+
+    path = "https://res.cloudinary.com/dp6zhyocx/image/upload/v1662046666/pqwya0pvqts4ubv0eqi6.jpg"
+    file = URI.open(path)
+    photo.attach(io: file, filename: "userphoto.png", content_type: "image/png")
+    save
+  end
 end
