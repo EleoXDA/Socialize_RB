5623d68f85fd9b6b94911ae1d3a656899fa82280
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-15 19:55:54 +0200

    first pundit policy -> user

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,12 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def show?
+    return true
+  end
+end
