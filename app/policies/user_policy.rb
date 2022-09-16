d7b0df38e6c9f6a1a538b88d4634d4f9e3557cf3
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-17 00:07:12 +0200

    ADD user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,28 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
 
   def show?
     return true
+  end
+
+  def new?
+    return create?
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    return update?
+  end
+
+  def update?
+    return record.user == user
   end
 end
