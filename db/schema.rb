37a3af4a850000aba4937223962d9b77d55e51d0
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-17 01:40:34 +0200

    changes to schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_074133) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_231733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_074133) do
     t.time "time"
     t.string "location"
     t.string "image"
+    t.string "nickname"
     t.index ["users_id"], name: "index_events_on_users_id"
   end
 
