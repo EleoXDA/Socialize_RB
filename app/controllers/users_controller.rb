e2538c291b2a77cf5640f82ec00d23fe0617e428
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-16 21:10:00 +0200

    delete orphan line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,7 @@ class UsersController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { user: user }),
-        image_url: helpers_asset_url("")
+        info_window: render_to_string(partial: "info_window", locals: { user: user })
       }
     end
   end
