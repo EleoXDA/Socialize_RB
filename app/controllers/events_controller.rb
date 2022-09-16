62611cf1769c3b84fe37e1939e07deed43829406
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-17 01:40:03 +0200

    Add euthoring user to event in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.user = current_user
     respond_to do |format|
       if @event.save
         format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
