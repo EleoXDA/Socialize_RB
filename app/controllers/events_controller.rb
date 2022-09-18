4fcae531f64738161c2f4e4125019603cad1b8d5
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-18 21:41:36 +0200

    authorize events when needed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,23 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+
   def index
     @events = policy_scope(Event).all
   end
 
   def show
-    @event = Event.find(params[:id])
+    authorize @event
   end
 
   def new
-    @event = Event.new
+    @event = Event.new(event_params)
+    authorize @event
   end
 
   def create
     @event = Event.new(event_params)
-    @event.user = current_user
+    authorize @event
+    # @event.nickname = current_user.nickname
     respond_to do |format|
       if @event.save
         format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
@@ -25,9 +29,25 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    authorize @event
+  end
+
+  def update
+    authorize @event
+  end
+
+  def destroy
+    authorize @event
+  end
+
   private
 
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
   def event_params
-    params.require(:event).permit(:title, :theme, :price, :photo, :description, :date, :time, :location)
+    params.require(:event).permit(:title, :theme, :price, :photo, :description, :date, :time, :location, :event)
   end
 end
