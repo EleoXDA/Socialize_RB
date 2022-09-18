b7d1bbec35a02a806526c84d8b212a3e35eabff5
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-18 21:38:48 +0200

    Rescue pundit to view pages that are not permitted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,11 @@ class ApplicationController < ActionController::Base
   end
 
   # Uncomment when you *really understand* Pundit!
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_to(root_path)
-  # end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 
   private
 
